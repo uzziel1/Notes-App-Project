88fe0782e99f6875d1f8f95c07360e9cc5b573bf
Uzziel Gonzalez <uzzielgonzalez247@gmail.com>
2024-07-20 23:31:23 -0400

    Implemented Note Editor html and began note editing DOM

--- public/scripts/note-editor.js
+++ public/scripts/note-editor.js
@@ -7,3 +7,51 @@ const { noteId, folderId } = {
 };
 
 const note = folders.getNote(folderId, noteId);
+
+let optionsButtons = document.querySelectorAll('option-button');
+let advanceOptionButton = document.querySelectorAll('.adv-option-button');
+let fontName = document.getElementById('fontName');
+let fontSizeRef = document.getElementById('fontSize');
+let writingArea = document.getElementById('text-input');
+let linkButton = document.getElementById('createLink');
+let alignButtons = document.querySelectorAll('.align');
+let spacingButtons = document.querySelectorAll('.spacing');
+let formatButtons = document.querySelectorAll('.format');
+let scriptButtons = document.querySelectorAll('.script');
+
+//List of fontlist
+
+let fontList = [
+  'Arial',
+  'Verdana',
+  'Times New Roman',
+  'Garamond',
+  'Georgia',
+  'Courier New',
+  'Cursive',
+];
+
+//Initial Settings
+const initializer = () => {
+  highlighter(alignButtons, true);
+  highlighter(spacingButtons, true);
+  highlighter(formatButtons, false);
+  highlighter(scriptButtons, true);
+};
+
+//Highlighter clicked button
+const highlighter = (className, needsRemoval) {
+    className.forEach((button) => {
+        button.addEventListener('click', () => {
+            // needsRemoval = true means only one button should be highlighted and the other would be normal
+            if (needsRemoval) {
+                let alreadyActive = false;
+                //If currently clicked button is alreadyActive 
+                if (button.classList.contains('active')) {
+                    alreadyActive = true;
+                }
+            }
+        })
+    })
+}
+
